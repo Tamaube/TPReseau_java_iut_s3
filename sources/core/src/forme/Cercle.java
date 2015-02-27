bf82b075b24d6b2f6270387fc4b3eb81f725e552
Elisa Lescarret <lescarretelisa@yahoo.fr>
2015-02-27 11:20:05 +0100

    commit 2

---
+++ sources/core/src/forme/Cercle.java
@@ -0,0 +1,17 @@
+package forme;
+
+import Outils.Point;
+
+public class Cercle {
+	private Point<Float> position;
+	public static float taille = 10;
+	
+	public Cercle (Point<Float> position){
+		this.position = position;
+	}
+
+	public Point<Float> getPosition() {
+		return position;
+	}
+	
+}
